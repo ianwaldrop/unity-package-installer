75dd6f07b3fe4cc7e2c04bd059c7af553d6d3357
Ian Waldrop <waldropij@gmail.com>
2016-02-15 16:51:53 -0800

    add space where space should exist

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ var installer = require('./../index.js');
 
 const PACKAGE_NAME = "test-package";
 
-installer.install(__dirname, PACKAGE_NAME, function(err, dir){
+installer.install(__dirname, PACKAGE_NAME, function(err, dir) {
 	if (err) {
 		console.error(err);
 		console.log("The most common reason for this is running the test from outside of a Unity project folder.");
