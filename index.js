ea1a4890978db102753325ae00ee23e76d932d15
Ian Waldrop <waldropij@gmail.com>
2016-02-16 16:42:16 -0800

    #2 place special folders properly within the project

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var findParent = require('find-parent-dir')
-  , mkdirp = require('mkdirp')
-  , path = require('path')
-  , ncp = require('ncp')
-;
+var findParent, mkdirp, dive, path, ncp, colors;
 
-const PACKAGES_FOLDER_NAME = 'Packages';
+// we do this try catch in order to work around the fact that
+// some packages aren't able to be found when processing
+// postinstall for the root package.
+try {
+  findParent = require('find-parent-dir');
+  mkdirp = require('mkdirp');
+  colors = require('colors');
+  dive = require('dive');
+  path = require('path');
+  ncp = require('ncp');
+}
+catch (err) {
+  // console.error('> ' + err + '. If this occurs on the root package, then it can be safely ignored.\n');
+  process.exit(0);
+}
 
 function handleError(err, callback) {
 	if (callback) {
@@ -14,6 +24,24 @@ function handleError(err, callback) {
 		console.error(err)
 		process.exit(1)
 	}
+}
+
+// TODO externalize this
+const RULES = {
+  folders: {
+    installAtRoot: [
+      'Editor Default Resources',
+      'WebPlayerTemplates',
+      'Standard Assets',
+      'StreamingAssets',
+      'Plugins',
+      'Gizmos',
+    ],
+    nested: [
+      'Resources',
+      'Editor',
+    ]
+  }
 }
 
 function getProjectRootFolder(callback) {
@@ -36,26 +64,51 @@ autoinstall = function() {
 install = function (sourceFolder, packageName, callback) {
 	getProjectRootFolder(function (err, dir) {
 		if (err) handleError(err, callback);
-
 		if (dir) {
-			var destination = path.join(dir, 'Assets', 'Packages', packageName);
+      const PACKAGES_FOLDER_NAME = 'Packages';
+      const ASSETS_FOLDER = path.join(dir, 'Assets');
+      const PACKAGE_FOLDER_INSTALL_PATH = path.join(ASSETS_FOLDER, PACKAGES_FOLDER_NAME, packageName);
+      const DIVE_OPTIONS = {
+        recursive: false,
+        all: false,
+        directories: true,
+        files: false
+      };
 
-			// create folder hierarchy if necessary
-		  mkdirp(destination, function (err) {
-			  if (err) handleError(err, callback);
+      function getFolderName(folder) {
+        var folderParts = folder.split(path.sep);
+        return folderParts[folderParts.length - 1];
+      }
 
-        console.log('> INSTALLED: Assets/Packages/' + packageName);
+      function getTarget(folderName) {
+        return path.join(RULES.folders.installAtRoot.indexOf(folderName) >= 0
+          ? ASSETS_FOLDER : PACKAGE_FOLDER_INSTALL_PATH, folderName);
+      }
 
-				// copy source folder to destination
-			  ncp(sourceFolder, destination, function (err) {
-				  if (err) handleError(err, callback);
-				  if (callback) callback(null, destination);
-			  });
-		  });
+      dive(sourceFolder, DIVE_OPTIONS, function(err, folder) {
+        if (err) handleError(err, callback);
+        var folderName = getFolderName(folder);
+        var target = getTarget(folderName);
+        copy(folder, target, callback);
+      });
+
+      var message = 'Installed ' + packageName;
+      console.log('> ' + message.cyan);
+      if (callback) callback(null, PACKAGE_FOLDER_INSTALL_PATH);
 		}
 		else {
       handleError("Failed to install. Reason: No unity project found to install into!", callback);
 		}
+  });
+}
+
+function copy(source, target, callback) {
+  mkdirp(target, function (err) {
+    if (err) handleError(err, callback);
+    ncp(source, target, function (err) {
+      if (err) handleError(err, callback);
+      // console.log('copying from [' + source + ']\n          to [' + target + ']');
+    });
   });
 }
 
