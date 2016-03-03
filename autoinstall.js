a1f8e8ecedc01c4e64009a8bd2aaa253a830ab74
Ian Waldrop <waldropij@gmail.com>
2016-03-03 15:37:32 -0800

    default source folder to `source` and install path to `package name`

--- autoinstall.js
+++ autoinstall.js
@@ -1,9 +1,6 @@
 var installer = require('../unity-package-installer');
 
 var env = process.env;
-var sourceFolder = env.npm_package_config_unity_package_installer_source;
-if (sourceFolder) {
-  var packageName = env.npm_package_name;
-  var destination = env.npm_package_config_unity_package_installer_path || packageName;
-  installer.install(sourceFolder, destination);
-}
+var sourceFolder = env.npm_package_config_unity_package_installer_source || `source`;
+var destination = env.npm_package_config_unity_install_path || env.npm_package_name;
+installer.install(sourceFolder, destination);
